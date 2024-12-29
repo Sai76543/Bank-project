4ef9bd73d4bd5794e66541db95d2b382a3cbb37b
Mikhail Otnyakin <otnyakin01@mail.ru>
2024-12-29 07:40:24 +0300

    fix_code

--- src/masks.py
+++ src/masks.py
@@ -6,9 +6,8 @@ def get_mask_card_number(card_number: Union[str]) -> Union[str]:
     принимает параметр card_number:
     возвращает маску номера карты
     """
-    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
-
-print(get_mask_card_number("7000792289606361")) # входной аргумент
+    masked_number = card_number[0:-12] + " " + "** ****" + card_number[-4:]
+    return masked_number
 
 
 def get_mask_account(account_number: Union[str]) -> Union[str]:
@@ -16,6 +15,5 @@ def get_mask_account(account_number: Union[str]) -> Union[str]:
     принимает параметр account_number:
     возвращает маску номера счета
     """
-    return f"**{account_number[-4:]}"
-
-print(get_mask_account("73654108430135874305")) # входной аргумент
+    masked_number = account_number[:4] + " " + "**" + account_number[-4:]
+    return masked_number
---
+++ src/widget.py
@@ -0,0 +1,28 @@
+from src.masks import get_mask_card_number, get_mask_account
+from typing import Union
+
+
+def mask_account_card(card_number: Union[str]) -> Union[str]:
+    """ Функция, которая маскирует номер счета или карты"""
+    if "Счет" in card_number or "Счёт" in card_number:
+        return get_mask_account(card_number)
+
+    elif "Visa" in card_number or "Maestro" in card_number:
+        return get_mask_card_number(card_number)
+
+
+def get_date(user_date: Union[str]) -> Union[str]:
+    """Функция получения даты в определенном формате и возвращает в формате ДД.ММ.ГГГГ"""
+    # Добавляем импорт библиотеки datetime
+    import datetime
+    # Преобразуем строку в объект datetime
+    date_format = datetime.datetime.strptime(user_date, "%Y-%m-%dT%H:%M:%S.%f")
+    # Форматируем дату в строку "ДД.ММ.ГГГГ"
+    new_date = date_format.strftime("%d.%m.%Y")
+    # Возвращаем отформатированную дату
+    return new_date
+
+
+print(mask_account_card("Visa Platinum 1234567891234567"))
+print(mask_account_card("Счет 35383033474447895560"))
+print(get_date("2024-03-11T02:26:18.671407"))
