7bcb9d4444e3d4a36a7a51586cb64637c2b90779
Mikhail Otnyakin <otnyakin01@mail.ru>
2025-01-21 13:18:04 +0300

    tests and code formatting

--- src/masks.py
+++ src/masks.py
@@ -6,8 +6,11 @@ def get_mask_card_number(card_number: Union[str]) -> Union[str]:
     принимает параметр card_number:
     возвращает маску номера карты
     """
-    masked_number = card_number[0:-10] + " " + "** ****" + card_number[-4:]
-    return masked_number
+    if card_number != "" and len(card_number) == 16:
+        masked_number = card_number[0:-10] + " " + "** ****" + card_number[-4:]
+        return masked_number
+    else:
+        return ("Введите корректный номер")
 
 
 def get_mask_account(account_number: Union[str]) -> Union[str]:
@@ -15,5 +18,11 @@ def get_mask_account(account_number: Union[str]) -> Union[str]:
     принимает параметр account_number:
     возвращает маску номера счета
     """
-    masked_number = account_number[:4] + " " + "**" + account_number[-4:]
-    return masked_number
+    if account_number != "" and len(account_number) == 20:
+        masked_number = account_number[:4] + " " + "**" + account_number[-4:]
+        return masked_number
+    else:
+        return "Введите корректный номер"
+
+print(get_mask_card_number("1111111111232123"))
+print("hello")
